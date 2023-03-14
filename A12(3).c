5669bc32edc6794f822d12e28b2bddab7abc60ba
anumeena <111556950+anu6376081311@users.noreply.github.com>
2023-03-14 23:20:34 +0530

    Add files via upload

---
+++ A12(3).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void oddN(int);
+int main()
+{
+    int x;
+    printf("Enter anumber :");
+    scanf("%d",&x);
+    oddN(x);
+    return 0;
+
+}
+void oddN(int n)
+{
+    if(n>=1)
+
+        {
+
+            oddN(n-1);
+            printf("%d " ,2*n-1);
+        }
+
+}

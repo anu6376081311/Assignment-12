f4eab091e418c80b207544c28d8cdb4625c911c3
anumeena <111556950+anu6376081311@users.noreply.github.com>
2023-03-14 23:21:18 +0530

    Add files via upload

---
+++ A12(5).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void EvenN(int);
+int main()
+{
+    int x;
+    printf("Enter anumber :");
+    scanf("%d",&x);
+    EvenN(x);
+    return 0;
+
+}
+void EvenN(int n)
+{
+    if(n>=1)
+
+        {
+
+            EvenN(n-1);
+            printf("%d " ,2*n);
+        }
+
+}

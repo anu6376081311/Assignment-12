bc2e673cd21e145c0965d9d2168632b464d3eae2
anumeena <111556950+anu6376081311@users.noreply.github.com>
2023-03-14 23:22:36 +0530

    Add files via upload

---
+++ A12(9).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void binary(int n);
+int main()
+{
+    int x;
+    printf("Enter the number which you want binary :");
+    scanf("%d",&x);
+    binary(x);
+    return 0;
+
+}
+void binary(int n)
+{
+     if(n==0)
+     return;
+    
+   binary(n/8);
+   printf("%d",n%8);
+}
